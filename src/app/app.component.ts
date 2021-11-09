6db029fbd5edfc9e46702dc7b41fa8fafe28f63a
Poschner <marie.poschner@atos.net>
2021-11-09 12:55:15 +0100

    Karussell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bildkarussell';
+  counter = 0;
+
+  Bilder:string[] = ["https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954_960_720.jpg", "https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636_960_720.jpg"];
+
+  onNext(){
+    if(this.counter < this.Bilder.length-1){
+      this.counter++;
+      console.log(this.counter);
+    }else{
+      this.counter = 0;
+    } 
+  }
 }
