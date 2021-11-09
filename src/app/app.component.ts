053be54aded6bf1702ba1a6c5a0542d2763bbada
Poschner <marie.poschner@atos.net>
2021-11-09 15:09:15 +0100

    AddImage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Bildkarussell';
   counter = 0;
-
   Bilder:string[] = ["https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954_960_720.jpg", "https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636_960_720.jpg"];
 
   onNext(){
@@ -18,5 +17,12 @@ export class AppComponent {
     }else{
       this.counter = 0;
     } 
+  }
+
+  addImage(){
+    let url=(document.getElementById("NewUrl") as HTMLInputElement).value
+    console.log(url);
+    //let temp = url.endsWith(".jgp");
+    this.Bilder.push(url);
   }
 }
